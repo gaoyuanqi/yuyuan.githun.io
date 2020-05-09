9f290d316e93b010a3f76cf6a4aeb456c4593e09
雨园 <47663624+gaoyuanqi@users.noreply.github.com>
2020-05-09 20:10:32 +0800

    Update time.js

--- js/time.js
+++ js/time.js
@@ -1,9 +1,15 @@
 
 <!--Hide the script from old browsers
   now = new Date();
+  hours = now.getHours();
+  mins = now.getMinutes();
+  secs = now.getSeconds();
   localtime = now.toString();
   utctime = now.toGMTString();
-  document.write(document.lastModified);
+  document.write(document.lastModified + "<br/>");
   document.write("<strong>Local time:</strong>" + localtime + "<br/>");
   document.write("<strong>UTC time:</strong>" + utctime);
+  document.write("<h1>");
+  document.write(hours + ":" + mins +":" + secs);
+  document.write("</h1>");
   // Stop hiding the script -->
