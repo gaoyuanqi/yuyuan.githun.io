08c6a658aab2f7fd807b16e87e04f07000004fc0
雨园 <47663624+gaoyuanqi@users.noreply.github.com>
2020-05-10 17:19:47 +0800

    Update time.js

--- js/time.js
+++ js/time.js
@@ -13,8 +13,16 @@ document.write("<h1>" + hours + ":" + mins +":" + secs + "</h1>");
 document.write("<strong>Local time:</strong>" + localtime + "<br/>");
 */
 
+/*百度网站统计代码*/
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?0abbef102c5013638e122e5e9f0fb3e0";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
 
-
+/*显示中国标准时间*/
 now = new Date();
 localtime = now.toString();
 document.write(localtime + "<br/>" + "<br/>");
